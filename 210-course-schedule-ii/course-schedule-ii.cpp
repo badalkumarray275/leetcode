de2f921f4431a268ee66a0c596ec402b61de8baa
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-07-19 21:10:43 +0530

    Time: 25 ms (59.53%) | Memory: 13.8 MB (63.27%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,46 +2,48 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         
-       vector<int> adj[numCourses];
+        
+        vector<int> adj[numCourses];
         for(auto it:prerequisites)
         {
             adj[it[1]].push_back(it[0]);
-            
+        }
+        vector<int>res;
+        queue<int>q;
+        vector<int>indegree(numCourses,0);
+        for(int i=0;i<numCourses;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
         }
 
-        queue<int>q;
-       vector<int>ans;
-	    vector<int>indegree(numCourses,0);
-	    for(int i=0;i<numCourses;i++)
-	    {
-	        for(auto it:adj[i])
-	        {
-	            indegree[it]++;
-	        }
-	    }
-	    for(int i=0;i<numCourses;i++)
-	    {
-	        if(indegree[i]==0)
-	        {
-	            q.push(i);
-	        }
-	    }
-	    while(!q.empty())
-	    {
-	        int node = q.front();
-            ans.push_back(node);
-	        q.pop();
-	        for(auto it:adj[node])
-	        {
-	            indegree[it]--;
-	            if(indegree[it]==0)
-	            {
-	                q.push(it);
-	            }
-	            
-	        }
-	    }
-       if(ans.size() ==numCourses ) return ans;
-       return {};
+        while(!q.empty())
+        {
+            int node = q.front();
+            res.push_back(node);
+            q.pop();
+
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+
+        if(res.size()== numCourses) return res;
+
+        return {};
     }
 };
