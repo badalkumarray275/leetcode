32637bd131b56e1e90319f519183aacdbdc2094c
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-07-24 19:36:22 +0530

    Time: 136 ms (71.30%) | Memory: 42.6 MB (57.36%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,103 @@
+class DisjointSet
+{
+  
+    public:
+    vector<int>parent,rank,size;
+    DisjointSet(int n)
+    {
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+        }
+    }
+    int findUpar(int node)
+    {
+        if(node == parent[node])
+        {
+            return node;
+        }
+        return parent[node]=   findUpar(parent[node]);
+    }
+    void unionByRank(int u,int v)
+    {
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+
+        if(ulp_u==ulp_v) return ;
+
+        if(rank[ulp_u]<rank[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+
+        }
+        else if(rank[ulp_u]>rank[ulp_v])
+        {
+            parent[ulp_v]= ulp_u;
+
+        }
+        else{
+             parent[ulp_v]= ulp_u;
+             rank[ulp_u]++;
+        }
+
+    }
+    void unionBySize(int u,int v)
+    {
+         int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+
+        if(ulp_u==ulp_v) return ;
+        if(size[ulp_u]<size[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v] += size[ulp_u];
+
+        }
+        else
+        {
+             parent[ulp_v]=ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int extraEdges = 0;
+        for(auto it: connections)
+        {
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUpar(u) == ds.findUpar(v))
+            {
+                extraEdges++;
+            }
+            else
+            {
+                ds.unionByRank(u,v);
+            }
+        }
+        int cnC =0;
+         for(int i=0;i<n;i++)
+        {
+            if(ds.findUpar(i)==i)
+            {
+                cnC++;
+            }
+        }    
+      int res = cnC-1;
+        if(extraEdges>= res)
+        {
+            return res;
+        }
+        else 
+        {
+            return -1;
+        }
+    }
+};
