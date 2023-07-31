117ce6b3a680a44064fa37d1b401ea8bb4fd1c06
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-07-31 19:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (57.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -19,18 +19,31 @@ public:
   
     //  ---> memoization
         
-        vector<int> dp(n+1,-1);
+        // vector<int> dp(n+1,-1);
      
 
         // --> tabulation
 
-         dp[0] =1;
-         dp[1] =1;
+        //  dp[0] =1;
+        //  dp[1] =1;
 
-         for(int i=2;i<=n;i++)
-         {
-             dp[i] = dp[i-1]+ dp[i-2];
-         }
-         return dp[n];
+        //  for(int i=2;i<=n;i++)
+        //  {
+        //      dp[i] = dp[i-1]+ dp[i-2];
+        //  }
+        //  return dp[n];
+
+        //  ---> space optimization
+ 
+
+        int prev2 =1;
+        int  prev =1;
+        for(int i=2;i<=n;i++)
+        {
+           int curr = prev2 + prev;
+           prev2 = prev;
+           prev = curr;
+        }
+        return prev;
     }
 };
