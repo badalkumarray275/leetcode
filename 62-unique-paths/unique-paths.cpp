d24634a2216e6e7d470b0509ecda12435fcc5ccb
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-04 03:14:33 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (48.10%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,67 @@
+class Solution {
+    private:
+    int f(int row,int col, vector<vector<int>>& dp)
+    {
+        if(row ==0 && col ==0)return 1;
+        if(row <0 || col <0) return 0;
+
+         int up = f(row-1,col,dp);
+         int left = f(row,col-1,dp);
+
+         return dp[row][col] = up + left;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+
+        // return f(m-1,n-1,dp);
+       int mod = 2e9;
+       
+        for(int row=0;row<m;row++)
+        {
+            for(int col =0;col <n;col++)
+            {
+                if(row ==0 && col ==0) 
+                {
+                    dp[row][col] = 1;
+                }   
+                    else 
+                    {
+                        int up =0,left =0;
+                        if(row>0)
+                        {
+                           up  += dp[row-1][col]; 
+                        }
+                        if(col >0)
+                        {
+                            left += dp[row][col-1];
+                        }
+                        dp[row][col] = (up + left)%mod;
+
+                    }
+                
+            }
+        }
+        return (dp[m-1][n-1])%mod;
+    //    int mod = 2e9;
+    //     vector<int> prev(n,0);
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         vector<int> temp(n,0);
+    //         for(int j=0;j<m;j++)
+    //         {
+    //             if(i ==0 && j==0)
+    //             {
+    //                temp[j] =1;
+    //             }
+    //             else 
+    //             {
+    //               temp[j]  = ((prev[j])%mod + (temp[j-1])%mod)%mod;
+    //             }
+    //         }
+    //         prev = temp;
+    //     }
+    //     return (prev[n-1])%mod;
+    }
+};
