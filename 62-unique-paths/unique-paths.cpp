ee9af51f117073c2c539a9d963943f7f8a50c5ce
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-04 03:20:16 +0530

    Time: 3 ms (37.55%) | Memory: 6.3 MB (66.28%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
     private:
-    int f(int row,int col, vector<vector<int>>& dp)
-    {
-        if(row ==0 && col ==0)return 1;
-        if(row <0 || col <0) return 0;
+    // int f(int row,int col, vector<vector<int>>& dp)
+    // {
+    //     if(row ==0 && col ==0)return 1;
+    //     if(row <0 || col <0) return 0;
 
-         int up = f(row-1,col,dp);
-         int left = f(row,col-1,dp);
+    //      int up = f(row-1,col,dp);
+    //      int left = f(row,col-1,dp);
 
-         return dp[row][col] = up + left;
-    }
+    //      return dp[row][col] = up + left;
+    // }
 public:
     int uniquePaths(int m, int n) {
         
