95bfeefadf55aa13f4a3177ec9ad9619b07b38b6
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-05 00:31:26 +0530

    Time: 3 ms (37.55%) | Memory: 6.2 MB (69.31%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,73 +1,73 @@
 class Solution {
     private:
-    // int f(int row,int col, vector<vector<int>>& dp)
-    // {
-    //     if(row ==0 && col ==0)return 1;
-    //     if(row <0 || col <0) return 0;
+    int f(int i,int j, vector<vector<int>>& dp)
+    {
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        
+        if(dp[i][j] !=-1) return dp[i][j];
+        int up = f(i-1,j,dp);
+        int left = f(i,j-1,dp);
 
-    //      int up = f(row-1,col,dp);
-    //      int left = f(row,col-1,dp);
-
-    //      return dp[row][col] = up + left;
-    // }
+        return dp[i][j] = up + left;
+    }
 public:
     int uniquePaths(int m, int n) {
         
-    //     vector<vector<int>> dp(m,vector<int>(n,-1));
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // //   return f(m-1,n-1,dp);
 
-    //     // return f(m-1,n-1,dp);
-       int mod = 2e9;
-       
-    //     for(int row=0;row<m;row++)
-    //     {
-    //         for(int col =0;col <n;col++)
-    //         {
-    //             if(row ==0 && col ==0) 
-    //             {
-    //                 dp[row][col] = 1;
-    //             }   
-    //                 else 
-    //                 {
-    //                     int up =0,left =0;
-    //                     if(row>0)
-    //                     {
-    //                        up  += dp[row-1][col]; 
-    //                     }
-    //                     if(col >0)
-    //                     {
-    //                         left += dp[row][col-1];
-    //                     }
-    //                     dp[row][col] = (up + left)%mod;
-
-    //                 }
-                
-    //         }
-    //     }
-    //     return (dp[m-1][n-1])%mod;
-    //    int mod = 2e9;
+        // // dp[0][0] = 1;
+        int mod = 2e9;
+        // for(int i=0;i<m;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(i==0 && j==0) 
+        //         {
+        //             dp[0][0] =1;
+        //         }
+        //         else
+        //         {
+        //             int up =0,left =0;
+        //             if(i>0)
+        //             {
+        //                 up += dp[i-1][j];
+        //             }
+        //             if(j>0)
+        //             {
+        //                 left += dp[i][j-1];
+        //             }
+        //             dp[i][j] = (up + left)%mod;
+        //         }
+        //     }
+        // }
+        // return (dp[m-1][n-1])%mod;
         vector<int> prev(n,0);
+
         for(int i=0;i<m;i++)
         {
             vector<int> temp(n,0);
             for(int j=0;j<n;j++)
             {
-                if(i ==0 && j==0)
+                if(i==0 && j==0)
                 {
-                   temp[j] =1;
+                    temp[0] =1;
                 }
                 else 
                 {
-                    int up =0,left =0;
+                    int up = 0,left =0;
 
-                   if(i>0)
-                   {
-                       up = up+ prev[j];
-                   }
-                   if(j>0)
-                   {
-                       left = left + temp[j-1];
-                   }
-                   temp[j]  = ((left)%mod + (up)%mod)%mod;
+                    if(i>0) 
+                    {
+                        up += prev[j];
+                    }
+                    if(j>0)
+                    {
+                        left += temp[j-1];
+                    }
+
+                    temp[j] = (up + left)%mod;
                 }
             }
             prev = temp;
