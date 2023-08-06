db1d16c967179912f1f80fc978f4c321bb6ed627
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-06 18:57:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (80.92%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,77 +1,65 @@
 class Solution {
+private:
+int f(int row,int col,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp)  
+{
+    if(row>=0 && col>=0 && obstacleGrid[row][col]==1) return 0;
+    if(row==0 && col ==0) return 1;
+    if(row<0 || col<0) return 0;
+     
+     if(dp[row][col] != -1) return dp[row][col];
+    int up = f(row-1,col,obstacleGrid,dp);
+    int left = f(row,col-1,obstacleGrid,dp);
 
-    private:
-    int f(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp)
-    {
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
-        if(dp[i][j] !=-1) return dp[i][j];
-        int up = f(i-1,j,obstacleGrid,dp);
-        int left = f(i,j-1,obstacleGrid,dp);
-
-        return dp[i][j] = up + left;
-    }
+    return  dp[row][col] = up + left;
+}
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-         
-         int m = obstacleGrid.size();
-         int n = obstacleGrid[0].size();
-        //  vector<vector<int>> dp(m,vector<int>(n,-1));
-        // //  return f(m-1,n-1,obstacleGrid,dp);
-         int mod = 2e9;
-        //  if(obstacleGrid[0][0] ==1) return 0;
-        // for(int i=0;i<m;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         if(i==0 && j==0) 
-        //         {
-        //             dp[i][j] =1;
+        
 
-        //         }
-        //         else if(obstacleGrid[i][j] ==1)
-        //         {
-        //             dp[i][j] =0;
-        //         }
-        //         else
-        //         {
-        //             int up =0,left =0;
-        //             if(i>0)
-        //             {
-        //                 up += dp[i-1][j];
-        //             }
-        //             if(j>0)
-        //             {
-        //                 left += dp[i][j-1];
-        //             }
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        // vector<vector<int>>dp(m,vector<int>(n,0));
+        // return f(m-1,n-1,obstacleGrid,dp);
+       
+    //    for(int row=0;row<m;row++)
+    //    {
+    //        for(int col =0;col<n;col++)
+    //        {
+    //             if(obstacleGrid[row][col]==1) dp[row][col] = 0;
+    //             else  if(row==0 && col ==0) dp[row][col] = 1;
+    //             else
+    //             { int up =0,left =0;
+    //               if(row-1>=0)  up += dp[row-1][col];
+    //               if(col-1>=0) left += dp[row][col-1];
 
-        //             dp[i][j] = (up + left)%mod;
-        //         }
-        //     }
-        // }
-        // return dp[m-1][n-1];
+    //                dp[row][col] = up + left;
+    //             }
+    //        }
+    //    }
+    //    return dp[m-1][n-1];
 
         vector<int>prev(n,0);
-        if(obstacleGrid[0][0]==1) return 0;
-        for(int i=0;i<m;i++)
+        
+        for(int row=0;row<m;row++)
         {
-            vector<int> temp(n,0);
-            for(int j=0;j<n;j++)
+            vector<int>temp(n,0);
+            for(int col =0;col<n;col++)
             {
-                if(i==0 && j==0) temp[j] =1;
-                else if(obstacleGrid[i][j] ==1) temp[j] =0;
-                else {
-                  
-                   int up = 0,left =0;
-                   if(i>0) up += prev[j];
-                   if(j>0)  left += temp[j-1];
+                
+                if(obstacleGrid[row][col]==1) temp[col] = 0;
+                else  if(row==0 && col ==0) temp[col] = 1;
+                else
+                { int up =0,left =0;
+                  if(row-1>=0)  up += prev[col];
+                  if(col-1>=0) left += temp[col-1];
 
-                   temp[j] = (up + left)%mod;
-                }
+                   temp[col] = up + left;
+                }  
             }
             prev = temp;
         }
+
         return prev[n-1];
+
     }
 };
