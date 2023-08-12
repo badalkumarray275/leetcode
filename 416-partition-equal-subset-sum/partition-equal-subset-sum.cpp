fe92d1c84875e5cef9f29518fc3df65be1fa4c8a
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-12 23:31:37 +0530

    Time: 211 ms (80.34%) | Memory: 9.6 MB (93.90%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,48 +1,41 @@
 class Solution {
-private:
-
-bool subsetSumToK(int n, int k, vector<int> &arr) {
-   
-      vector<vector<bool>> dp(n,vector<bool>(k+1,0));
-        for(int ind =0;ind<n;ind++)
-        {
-             dp[ind][0] = true;
-         }
-        
-       if(arr[0]<=k) dp[0][arr[0]]  = true;
-        for(int ind =1;ind<n;ind++)
-        {
-            for(int target =1;target<= k;target++)
-            {
-                  
-              bool notpick = dp[ind-1][target];
-              bool pick = false;
-              if(target>=arr[ind]) pick = dp[ind-1][target-arr[ind]];
-         
-                dp[ind][target] = pick | notpick;     
-            }
-        }
-        return dp[n-1][k];
-    
-
-
-}
 public:
     bool canPartition(vector<int>& nums) {
         
-        int n = nums.size();
-       
-	int sum = 0;
-	for(int i=0;i<n;i++)
-	{
-		sum += nums[i];
-	}
 
-	if(sum %2 !=0) return false;
+          int n = nums.size();
 
-	int target  = sum/2;
+          int t_sum =0;
 
-    return   subsetSumToK(n,target,nums); 
+          for(int i=0;i<n;i++)
+          {
+              t_sum += nums[i];
+          }
 
+          if(t_sum %2 != 0) return false;
+
+          int sum = t_sum/2;
+
+          vector<bool>prev(sum+1,0),curr(sum+1,0);
+
+           prev[0]= curr[0] = 1;
+
+          if(nums[0]<=sum) prev[nums[0]] =1;
+
+          for(int ind =1;ind<n;ind++)
+          {
+              for(int target =1;target<=sum;target++)
+              {
+                  bool notpick = prev[target];
+                  bool pick =false;
+
+                  if(nums[ind]<=target)  pick = prev[target-nums[ind]];
+
+                  curr[target] = notpick | pick;
+              }
+              prev = curr;
+          }
+
+          return prev[sum];
     }
 };
