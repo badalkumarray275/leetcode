85a23059fe13a8a3d38489f0f32b7ec0dbc65de4
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-13 22:24:01 +0530

    Time: 70 ms (85.55%) | Memory: 18.6 MB (43.42%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,86 @@
+class Solution {
+
+
+        private:
+    
+    int f(int ind,int target,vector<int>& coins, vector<vector<int>>& dp)
+    {
+        if(ind == 0) 
+        {
+            if(target % coins[0] == 0) return target/coins[0];
+            else  return 1e9;
+        }         
+            if(dp[ind][target]  !=-1) return dp[ind][target];
+            int notpick = 0 + f(ind-1,target,coins,dp);
+            
+            int pick = INT_MAX;
+            
+            if(coins[ind]<=target) pick = 1 + f(ind,target-coins[ind],coins,dp);
+            
+            return dp[ind][target] = min(notpick,pick);
+        
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        int n = coins.size();
+        // vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        //   int ans = f(n-1,amount,coins,dp);
+
+
+
+        //  for(int target =0;target<=amount;target++)
+        //  {
+        //       if(target % coins[0] == 0) dp[0][target] = target/coins[0];
+        //       else dp[0][target] = 1e9;
+        //  }
+         
+
+        //  for(int ind =1;ind<n;ind++)
+        //  {
+        //      for(int target =0;target<=amount;target++)
+        //      {
+        //          int notpick = 0 +dp[ind-1][target];
+            
+        //           int pick = INT_MAX;
+            
+        //            if(coins[ind]<=target) pick = 1 + dp[ind][target-coins[ind]];
+        //            dp[ind][target] = min(notpick,pick);
+        
+        //      }
+        //  }
+         
+        //  int ans = dp[n-1][amount];
+        
+
+        vector<int>prev(amount+1,0),curr(amount+1,0);
+          for(int target =0;target<=amount;target++)
+         {
+              if(target % coins[0] == 0) prev[target] = target/coins[0];
+              else prev[target] = 1e9;
+         }
+         
+
+         for(int ind =1;ind<n;ind++)
+         {
+             for(int target =0;target<=amount;target++)
+             {
+                 int notpick = 0 + prev[target];
+            
+                  int pick = INT_MAX;
+            
+                   if(coins[ind]<=target) pick = 1 + curr[target-coins[ind]];
+                   curr[target] = min(notpick,pick);
+        
+             }
+             prev = curr;
+         }
+         
+         int ans = prev[amount];
+
+
+
+          if(ans == 1e9) return -1;
+          else return ans;
+    }
+};
