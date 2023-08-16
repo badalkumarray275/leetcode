d49954dbd144f0dafa73ea75d27bf5fa560c6fe7
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-17 01:58:54 +0530

    Time: 7 ms (90.23%) | Memory: 6.4 MB (97.78%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,11 +2,11 @@ class Solution {
     private:
     int f(int i,int j,string word1, string word2,vector<vector<int>>& dp)
     {
-        if(i<0) return j+1;
-        if(j<0) return  i+1;
+        if(i==0) return j;
+        if(j==0) return  i;
         
         if(dp[i][j] != -1) return dp[i][j];
-        if(word1[i] == word2[j])
+        if(word1[i-1] == word2[j-1])
         {
             return dp[i][j] =  0 + f(i-1,j-1,word1,word2,dp);
         }
@@ -22,8 +22,36 @@ public:
 
         int n = word1.size();
         int m = word2.size();
-         vector<vector<int>>dp(n,vector<int>(m,-1));
+        //  vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-        return f(n-1,m-1,word1,word2,dp);
+        // return f(n,m,word1,word2,dp);
+
+        // for(int j=0;j<=m;j++) dp[0][j] = j;
+        // for(int i=0;i<=n;i++) dp[i][0] =i;
+        vector<int>prev(m+1,0),curr(m+1,0);
+         for(int j=0;j<=m;j++) 
+          {
+                prev[j] = j;
+          }
+
+        for(int i=1;i<=n;i++)
+        {
+            curr[0] = i;
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1] == word2[j-1])
+                {
+                  curr[j] =  0 +prev[j-1];
+                }
+                else
+               {
+                   curr[j] = 1 + min(curr[j-1] ,
+                 min(prev[j],prev[j-1]));
+               } 
+            }
+            prev = curr;
+        }
+        return prev[m];
+      
     }
 };
