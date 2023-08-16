f9525ab4ffefb902f68dadc389aed5c87e625c2a
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-16 22:14:31 +0530

    Time: 11 ms (92.67%) | Memory: 6.9 MB (89.98%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,35 +1,33 @@
 class Solution {
     private:
-
-        int longestCommonSubsequence(string text1, string text2) {
+      
+          int longestCommonSubsequence(string text1, string text2) {
         
         int n = text1.size();
         int m = text2.size();
+          vector<int>prev(m+1,0),curr(m+1,0);
+           for(int j =0;j<=m;j++) prev[j] =0;
+
+         for(int i=1;i<=n;i++)
+         { 
+             curr[0] =0;
+             for(int j=1;j<=m;j++)
+             {
+                     if(text1[i-1]==text2[j-1]) curr[j] = 1 + prev[j-1];
        
-        vector<int>prev(m+1,0),curr(m+1,0);
+                     else curr[j] = max(prev[j],curr[j-1]);
+             }
+             prev = curr;
+         }
 
-        for(int j=0;j<=m;j++) prev[j] =0;
-
-        for(int i =1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-       
-                if(text1[i-1]==text2[j-1])  curr[j] = 1 + prev[j-1];
-
-                 else curr[j] = max(prev[j],curr[j-1]);  
-            }
-            prev = curr;
-        }
         return prev[m];
-
     }
 public:
     int minDistance(string word1, string word2) {
         
-        int n = word1.size();
-        int m = word2.size();
+          int n = word1.size();
+          int m = word2.size();
 
-        return n + m -(2* longestCommonSubsequence(word1,word2));
+          return n + m - 2*longestCommonSubsequence(word1,word2);
     }
 };
