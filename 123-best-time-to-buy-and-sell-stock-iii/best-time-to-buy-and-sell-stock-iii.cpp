1a7f12d0700fb8cc845fe7eaf7b2197ce8fc858d
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-18 01:08:27 +0530

    Time: 297 ms (66.04%) | Memory: 132.7 MB (63.79%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,62 +1,23 @@
 class Solution {
-    private:
-    int f(int ind,int buy,int cap,vector<int>& prices,int n,vector<vector<vector<int>>>& dp)
+
+    int f(int ind,int tran,vector<int>& prices,int n,vector<vector<int>>& dp)
     {
-       if(ind == n ||  cap ==0) return 0;
-        
-        if(dp[ind][buy][cap] !=-1) return dp[ind][buy][cap];
-        if(buy)
+        if(ind == n || tran == 4) return  0;
+        if(dp[ind][tran] != -1) return dp[ind][tran];
+        if(tran %2 ==0)
         {
-            return dp[ind][buy][cap] = max(-prices[ind] + f(ind+1,0,cap,prices,n,dp), 
-            0 + f(ind+1,1,cap,prices,n,dp));
+            return dp[ind][tran] =  max(-prices[ind] + f(ind+1,tran+1,prices,n,dp) 
+            , 0 + f(ind+1,tran,prices,n,dp));
         }
-         
-         return dp[ind][buy][cap] =  max(prices[ind] + f(ind +1,1,cap-1,prices,n,dp) ,
-         0 + f(ind+1,0,cap,prices,n,dp));
-    }
+        
+        return dp[ind][tran] =  max(prices[ind] + f(ind+1,tran+1,prices,n,dp) 
+         , 0 + f(ind+1,tran,prices,n,dp));
+    }   
 public:
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
-        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        // return f(0,1,2,prices,n,dp);
-
-        // for(int buy = 0;buy<2;buy++)
-        // {
-        //     for(int cap = 0;cap<3;cap++)
-        //     {
-        //          dp[n][buy][cap] = 0;
-        //     }
-        // }
-
-        // for(int ind=0;ind<n;ind++)
-        // {
-        //     for(int buy =0;buy<2;buy++)
-        //     {
-        //         dp[ind][buy][0] = 0;
-        //     }
-        // }
-       vector<vector<int>>prev(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
-
-        for(int ind = n-1;ind>=0;ind--)
-        {
-            for(int buy =0;buy<2;buy++)
-            {
-                for(int cap =1;cap<3;cap++)
-                {
-                     if(buy)
-                     {
-                       curr[buy][cap] = max(-prices[ind] + prev[0][cap], 
-                       0 + prev[1][cap]);
-                     }
-                     else
-         
-                     curr[buy][cap] =  max(prices[ind] + prev[1][cap-1],
-                     0 + prev[0][cap]);
-                }
-            }
-            prev = curr;
-        }
-        return prev[1][2];
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return f(0,0,prices,n,dp);
     }
 };
