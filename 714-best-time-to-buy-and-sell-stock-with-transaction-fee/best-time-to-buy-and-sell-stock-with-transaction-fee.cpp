d2f68137da2b8180dc4ef938d3acdefc91a4bdac
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-18 22:00:02 +0530

    Time: 77 ms (98.39%) | Memory: 54.9 MB (98.21%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,33 +1,8 @@
 class Solution {
-   
-   private:
-    int f(int ind,int buy,vector<int>& prices, vector<vector<int>>& dp,int n,int fee)
-    {
-       
-        if(ind == n) return 0;
-         int profit =0;
-         if(dp[ind][buy]  !=-1) return dp[ind][buy]; 
-        if(buy)
-        {
-           profit =  max(-prices[ind] + f(ind+1,0,prices,dp,n,fee),
-           (0 + f(ind+1,1,prices,dp,n,fee)));
-
-        }
-        else
-        {
-            profit = max( (prices[ind] - fee + f(ind+1,1,prices,dp,n,fee)),
-             (0 + f(ind+1,0,prices,dp,n,fee)));
-        }
-        return dp[ind][buy] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices, int fee) {
-         
+              
            int n = prices.size();
-        //    vector<vector<int>>dp(n+1,vector<int>(2,0));
-        //    return f(0,1,prices,dp,n);
-        
-        // vector<int>ahead(2,0),curr(2,0);
           int aheadNotBuy =0;
           int   aheadBuy  =0;
  
@@ -37,13 +12,12 @@ public:
               {
                    
                    int currNotBuy = max(prices[ind] + aheadBuy ,0 +aheadNotBuy);
-                   int  currBuy = max(-prices[ind] -fee +  aheadNotBuy,0+aheadBuy);
+                   int  currBuy = max(-prices[ind]-fee +  aheadNotBuy,0+aheadBuy);
                    aheadNotBuy = currNotBuy;
                    aheadBuy = currBuy;
               }
             
           }
           return aheadBuy;
-
     }
 };
