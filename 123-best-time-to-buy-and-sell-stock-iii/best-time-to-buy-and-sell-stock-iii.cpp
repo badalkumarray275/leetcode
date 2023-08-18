9e02f7c43e6bb87f17b6975b07e059ae29ab90a7
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-18 19:52:53 +0530

    Time: 164 ms (86.11%) | Memory: 75.4 MB (82.32%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,49 @@
 class Solution {
 
-    int f(int ind,int tran,vector<int>& prices,int n,vector<vector<int>>& dp)
-    {
-        if(ind == n || tran == 4) return  0;
-        if(dp[ind][tran] != -1) return dp[ind][tran];
-        if(tran %2 ==0)
-        {
-            return dp[ind][tran] =  max(-prices[ind] + f(ind+1,tran+1,prices,n,dp) 
-            , 0 + f(ind+1,tran,prices,n,dp));
-        }
-        
-        return dp[ind][tran] =  max(prices[ind] + f(ind+1,tran+1,prices,n,dp) 
-         , 0 + f(ind+1,tran,prices,n,dp));
-    }   
+    private:
+     int f(int ind,int buy,int cap,vector<int>& prices,int n,vector<vector<vector<int>>>& dp)
+     {
+         if(ind == n || cap ==0) return 0;
+         
+         if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+         if(buy) 
+         {
+              return dp[ind][buy][cap] = max(-prices[ind] + f(ind+1,0,cap,prices,n,dp) ,
+               0 + f(ind+1,1,cap,prices,n,dp));
+         }
+         return dp[ind][buy][cap] =  max( prices[ind] + f(ind+1,1,cap-1,prices,n,dp) ,
+          0 + f(ind+1,0,cap,prices,n,dp));
+     }
 public:
     int maxProfit(vector<int>& prices) {
         
+
         int n = prices.size();
-        vector<vector<int>>dp(n,vector<int>(4,-1));
-        return f(0,0,prices,n,dp);
+        vector<vector<int>> front(2,vector<int>(3,0));
+          vector<vector<int>>curr(2,vector<int>(3,0));
+        // return f(0,1,2,prices,n,dp);
+
+  
+        for(int ind = n-1;ind>=0;ind--)
+        {
+            for(int buy =0;buy<2;buy++)
+            {
+                for(int cap =1;cap<3;cap++)
+                {
+                     if(buy) 
+                   {
+                       curr[buy][cap] = max(-prices[ind] + front[0][cap],
+                     0 + front[1][cap]);
+                   }
+                   else
+
+                      curr[buy][cap] =  max( prices[ind] + front[1][cap-1], 
+                     0 + front[0][cap]);
+                }
+            }
+            front = curr;
+        }
+         return front[1][2];
+        
     }
 };
