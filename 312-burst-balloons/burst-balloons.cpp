bd65fc150d635c479353de708690eb0df07c7c38
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2023-08-24 00:23:51 +0530

    Time: 272 ms (83.95%) | Memory: 10.4 MB (9.75%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,48 +1,44 @@
 class Solution {
-
     private:
-    int f(int i,int j,vector<int>& nums, vector<vector<int>>& dp)
+    int f(int i ,int j ,vector<int>& nums, vector<vector<int>>& dp)
     {
-        int n =  nums.size();
         if(i>j) return 0;
          
-
+         int maxi = INT_MIN;
          if(dp[i][j] != -1) return dp[i][j];
-        int maxi = -1e9;
-         
         for(int ind =i;ind<=j;ind++)
         {
-             int cost = nums[i-1]*nums[ind]*nums[j+1] + f(i,ind-1,nums,dp) + f(ind +1,j,nums,dp);
-             maxi = max(maxi,cost);
-
+            int cost = nums[i-1]*nums[ind]*nums[j+1] + f(i,ind-1,nums,dp) + f(ind+1,j,nums,dp);
+            maxi = max(maxi,cost);
         }
-        return  dp[i][j] = maxi;
+
+        return dp[i][j] = maxi;
     }
 public:
     int maxCoins(vector<int>& nums) {
         
-        int n = nums.size();
+        int n =  nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>>dp(n+2,vector<int>(n+2,0));  
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
         // return f(1,n,nums,dp);
 
-        for(int i = n;i>=1;i--)
+        for(int i= n;i>=1;i--)
         {
-             for(int j = 1;j<=n;j++)
-             {
-                 if(i>j) continue; 
-                   int maxi = -1e9;
-         
-                 for(int ind =i;ind<=j;ind++)
-                {
-                  int cost = nums[i-1]*nums[ind]*nums[j+1] + dp[i][ind-1] + dp[ind+1][j];
-                   maxi = max(maxi,cost);
+            for(int j =1;j<=n;j++)
+            {
+                if(i>j) continue;
+               int maxi = INT_MIN;
+               for(int ind =i;ind<=j;ind++)
+              {
+               int cost = nums[i-1]*nums[ind]*nums[j+1] + dp[i][ind-1]+ dp[ind+1][j];
+                 maxi = max(maxi,cost);
+              }
 
-                }
-                  dp[i][j] = maxi;
-             }
+               dp[i][j] = maxi;
+            }
         }
+
         return dp[1][n];
     }
 };
