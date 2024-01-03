f123043db08127e4fe19ada79e15b6aa2d6b2a84
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2024-01-03 14:50:58 +0530

    Time: 11 ms (47.26%) | Memory: 10.9 MB (14.05%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,18 +12,29 @@ public:
         
         // --> with tc 0(n) and sc 0(1)
 
-    ListNode* temp = new ListNode();
-    ListNode* curr = head;
-    while(curr != NULL)
-    {
-        if(curr->next == NULL)return false;
-        if(curr->next == temp)return true;
-        ListNode* next = curr->next;
-        curr->next = temp;
-        curr = next;
+    // ListNode* temp = new ListNode();
+    // ListNode* curr = head;
+    // while(curr != NULL)
+    // {
+    //     if(curr->next == NULL)return false;
+    //     if(curr->next == temp)return true;
+    //     ListNode* next = curr->next;
+    //     curr->next = temp;
+    //     curr = next;
         
-    }
-    return false;
+    // }
+    // return false;
 
+
+    //  -- with sc 0(n)
+
+        unordered_set<ListNode*>s;
+        for(ListNode* curr = head;curr != NULL;curr = curr->next)
+        {
+            if(s.find(curr) != s.end()) return true;
+            s.insert(curr);
+        }
+        return false;
+        
     }
 };
