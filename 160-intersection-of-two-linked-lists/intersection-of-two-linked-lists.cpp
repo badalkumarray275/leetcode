83a1b7df10737c9380df0cc640eacb54868404e3
badalkumarray275 <131524130+badalkumarray275@users.noreply.github.com>
2024-01-12 18:45:35 +0530

    Time: 35 ms (77.55%) | Memory: 14.9 MB (83.34%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -10,18 +10,44 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
-        // tc - o(m+n) , sc - o(m)
+        // tc - o(m+n) , sc - o(1)
 
-        unordered_set<ListNode*> s;
-        for(ListNode* curr = headA;curr != NULL;curr = curr->next)
-        {
-            s.insert(curr);
-        }
-        for(ListNode* curr = headB;curr != NULL;curr = curr->next)
-        {
-            if(s.find(curr) != s.end()) return curr;
-
-        }
-        return NULL;
+         int c1 =0,c2 = 0;
+         for(ListNode* curr = headA;curr != NULL;curr = curr->next)
+         {
+             c1++;
+         }
+         for(ListNode* curr = headB;curr != NULL;curr = curr->next)
+         {
+             c2++;
+         }
+         int d = abs(c1-c2);
+         ListNode* curr1 = headA;
+          ListNode* curr2 = headB;
+         if(c1>=c2)
+         {
+              for(int i = 1;i<=d;i++)
+              {
+                curr1 = curr1->next;
+              }
+         }
+         else
+         {
+              for(int i = 1;i<=d;i++)
+             {
+               curr2 = curr2->next;
+             }
+         }
+        
+        
+         while(curr1 != NULL && curr2 != NULL)
+         {
+             if(curr1 == curr2) return curr1;
+             curr1 = curr1->next;
+             curr2 = curr2->next;
+         }
+         return NULL;
+        
+       
     }
 };
